c8e9ad96a1485f6846018d17537624a26190661f
Crystal Duran <crystal12duran12@outlook.es>
2023-07-29 02:55:57 -0400

    proyecto django: con funciones y configuraciones basicas

---
+++ CalcDerivIntegrales/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from sympy import *
+
+# Create your views here.
+
+def calcular(request):
+    derivada_resultado = None
+    integral_resultado = None
+    punto_evaluacion = 0
+    a = 0
+    b = 0
+    f_LatexDerivada = None
+    f_LatexIntegralIndefinida = None
+
+    if request.method == 'POST':
+        if 'calcularDerivadaBtn' in request.POST:
+            derivada_input = request.POST['functionDerivada']
+            f_derivada = sympify(derivada_input)
+            x = symbols('x')
+            derivada_resultado = diff(f_derivada, x)
+            derivada_resultado = latex(derivada_resultado)
+            f_LatexDerivada = latex(f_derivada)
+        elif 'calcularIntegralIndefinidaBtn' in request.POST:
+            integral_input = request.POST['functionIntegralIndefinida']
+            f_integralIndefinida = sympify(integral_input)
+            x = symbols('x')
+            integral_resultado = integrate(f_integralIndefinida, x)
+            integral_resultado = latex(integral_resultado)
+            f_LatexIntegralIndefinida = latex(f_integralIndefinida)
+        
+
+
+        """
+        if 'derivada_form' in request.POST:
+            derivada_form = DerivativeForm(request.POST)
+                x = symbols('x')
+                expr = sympify(funcion)
+                derivada_resultado = diff(expr, x)
+                #derivada_resultado = diff(expr, x).subs(x, evaluation_point)
+        elif 'integral_form' in request.POST:
+            integral_form = IntegralForm(request.POST)
+            if integral_form.is_valid():
+                funcion = integral_form.cleaned_data['function']
+                #lower_limit = integral_form.cleaned_data['lower_limit']
+                #upper_limit = integral_form.cleaned_data['upper_limit']
+
+                x = symbols('x')
+                expr = sympify(funcion)
+                integral_resultado = integrate(expr, x)
+        """
+
+    context = {'derivative_result': derivada_resultado, 'integral_result' : integral_resultado,'f_derivada' : f_LatexDerivada, 'f_integralIndefinida': f_LatexIntegralIndefinida}
+    return render(request,'index.html',context)
+    
+    
